f66f7b5c1c02d0f1bb91739f77d99ddf5dd34ddf
Ola-Oluwajuwon <okayode986@stu.ui.edu.ng>
2025-09-20 14:18:27 +0100

    Feature: add the endpoint for adding favorite meal. Fix: remove a trailing whitespace for a module.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
-import { drizzle } from "drizzle-orm/neon-http ";
+import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import { ENV } from "../config/env.js";
-import * as schema from "./schema.js";
+import * as schema from "../src/db/schema.js";
 
 const sql = neon(ENV.DATABASE_URL);
 export const db = drizzle(sql, { schema });
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,56 @@
 import express from "express";
 import { ENV } from "../config/env.js";
+import { db } from "../config/db.js";
+import { favoritesTable } from "./db/schema.js";
 
 // Initialize Express app
 const app = express();
 const PORT = ENV.PORT || 3000;
 
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Health check endpoint
 app.get("/api/health", (req, res) => {
   res.status(200).json({ success: true, message: "API is healthy" });
-  // res.send("Hello World!");
+});
+
+app.post("/api/favorites", async (req, res) => {
+  // Logic to add a favorite recipe
+  try {
+    // Add logic to save the favorite recipe to the database
+    const { userId, recipeId, title, image, cookTime, servings } = req.body;
+
+    if (!userId || !recipeId || !title) {
+      return res.status(400).json({
+        success: false,
+        message: "Missing required fields",
+      });
+    }
+
+    const newFavorite = await db
+      .insert(favoritesTable)
+      .values({
+        userId,
+        recipeId,
+        title,
+        image,
+        cookTime,
+        servings,
+      })
+      .returning();
+
+    res.status(201).json({
+      success: true,
+      message: "Recipe added to favorites",
+      data: newFavorite,
+    });
+  } catch (error) {
+    console.error("Error adding favorite recipe:", error);
+    res
+      .status(500)
+      .json({ success: false, message: "Failed to add recipe to favorites" });
+  }
 });
 
 // Middleware to parse JSON requests
