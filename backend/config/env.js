f2bad60617289ef282561a042c53cc0ae7475c82
Ola-Oluwajuwon <okayode986@stu.ui.edu.ng>
2025-09-20 12:54:24 +0100

    Feature: setup db schema and connect it to Neon Severless Services.

--- backend/config/env.js
+++ backend/config/env.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const ENV = {
   PORT: process.env.PORT || 3000,
-  DATABASE_URL: process.env.DATABASE_URL || "mongodb://localhost:27017/myapp",
+  DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
   //   JWT_SECRET: process.env.JWT_SECRET || 'your_jwt_secret',
 };
