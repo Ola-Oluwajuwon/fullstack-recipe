82d7867c0b0fa9a3dc92d75be06f1619b9ccfcc8
Ola-Oluwajuwon <okayode986@stu.ui.edu.ng>
2025-09-26 12:36:17 +0100

    Feature: setup project themes and styles.

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
@@ -9,7 +10,19 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Hello, Expo!</Text>
+      <TextInput placeholder="Type here..." />
+      <TouchableOpacity
+        style={{
+          backgroundColor: "brown",
+          padding: 12,
+          borderRadius: 4,
+          marginTop: 12,
+        }}
+      >
+        <Text style={{ color: "white" }}>Submit</Text>
+      </TouchableOpacity>
+      <Link href="/about">Go to About</Link>
     </View>
   );
 }
